da94b109aa075242302b6ce752dc873d5ed5b696
Raziye Nikookolah <r.nikookolah@gmail.com>
2023-04-15 12:57:22 +0000

    "add a filehandler to save transactions in file using shelve module"

---
+++ hw11/handlers.py
@@ -0,0 +1,34 @@
+import shelve
+from datetime import date
+
+class FileHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def get_all_transactions(self):
+        with shelve.open(self.file_path)as db:
+            return list(db)
+
+    def append_transaction(self, item):
+        with shelve.open(self.file_path)as db:
+            db = db.update(item)
+
+    def search_transaction_by_date(self, start_date:date,end_date:date):
+        with shelve.open(self.file_path)as db:
+            founded_lst=[]
+            for item in db:               
+                if item.get["date"] >=start_date and item.get["date"]<=end_date:
+                    founded_lst.append(item)
+            return founded_lst
+
+    # def get_all_values(self):
+    #     with shelve.open(self.file_path)as db:
+    #         return list(db.values())
+
+    # def remove(self, key):
+    #     with shelve.open(self.file_path)as db:
+    #         if key in db.keys():
+    #             del db[key]
+    #             return True
+    #         else:
+    #             return False
--- hw11/personal_finance_manager.py
+++ hw11/personal_finance_manager.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 from transaction import Transaction,TransactionType
+from handlers import FileHandler
  
 class PersonalFinanceManager:
     lst_transactions = []
+    __db=FileHandler("storage/transactions.shelve")
     balance = 0
 
+    @classmethod
+    def get_all_transaction_from_file(cls):
+        cls.lst_transactions=cls.__db.get_all_transactions()
+    
     @classmethod
     def add_transaction(cls, transaction: Transaction) -> None:
         """Add a new transaction to the personal finance manager."""
@@ -16,24 +22,23 @@ class PersonalFinanceManager:
             raise ValueError("Invalid transaction type")
         
         dict_transaction={'type': transaction.type , 'date': transaction.date, 'amount': transaction.amount, 'category' :
-                        transaction.category, 'description': transaction.description}
-        cls.lst_transactions.append((dict_transaction, cls.balance))
+                        transaction.category, 'description': transaction.description,'balance':cls.balance}
+        cls.lst_transactions.append(dict_transaction)
+        cls.__db.append_transaction(dict_transaction)
+        
         
     
     @classmethod
     def show_transactions(cls ,start_date:str=None,end_date:str=None) -> None:
         """View all transactions between two dates."""
-        if start_date==None and end_date==None:#show all transactions
-            
-            for t in cls.lst_transactions:
-                print(t[0],t[1])
-        elif end_date==None:#user enter just start date, show transactions until now
-            
+        if start_date==None and end_date==None:# show all transactions            
+            lst_result=cls.__db.get_all_transactions()
+            print(lst_result)
+        elif end_date==None:# user enter just start date => show transactions until now            
             end_date=datetime.now()
-        else:#user enter start_date and end_date
-            
-            for t in cls.lst_transactions:#show transaction between 2 dates entered
-                transaction_date=t[0].get('date')
-                if transaction_date>=start_date and transaction_date<=end_date:
-                    print(t[0],t[1])
+            lst_result=cls.__db.search_transaction_by_date(start_date,end_date)
+            print(lst_result)
+        else:# user enter start_date and end_date
+            lst_result=cls.__db.search_transaction_by_date(start_date,end_date)
+            print(lst_result)
 
