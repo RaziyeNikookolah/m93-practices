39e31e4575366968e251c129b9f14f0e6d9e2a14
Raziye Nikookolah <r.nikookolah@gmail.com>
2023-04-18 12:44:39 +0000

    "using filter to falow best practice"

--- hw11/core/handlers.py
+++ hw11/core/handlers.py
@@ -3,30 +3,6 @@ from datetime import date
 from transaction.models import Transaction, TransactionType
 
 
-# class BalanceFileHandler:
-
-#     def __init__(self, file_path):
-#         self.file_path = file_path
-
-#     def get_overal_expence_balance(self):
-#         with shelve.open(self.file_path)as db:
-#             return db.get("OVERALL_EXPENCE_BALANCE", 0)
-
-#     def get_overal_income_balance(self):
-#         with shelve.open(self.file_path)as db:
-#             return db.get("OVERAL_INCONE_BALANCE", 0)
-
-#     def set_overal_expence_balance(self, balance):
-#         with shelve.open(self.file_path)as db:
-#             db = db.update({"OVERALL_EXPENCE_BALANCE": db.get(
-#                 "OVERALL_EXPENCE_BALANCE", 0)-balance})
-
-#     def set_overal_income_balance(self, balance):
-#         with shelve.open(self.file_path)as db:
-#             db = db.update({"OVERAL_INCONE_BALANCE": db.get(
-#                 "OVERAL_INCONE_BALANCE", 0)+balance})
-
-
 class TransactionsFileHandler:
     def __init__(self, file_path):
         self.file_path = file_path
@@ -41,15 +17,14 @@ class TransactionsFileHandler:
 
     def search_transaction_by_date_range(self, start_date: date, end_date: date):
         with shelve.open(self.file_path)as db:
-            founded_lst = []
-            for item in list(db.values()):
-                if item.date >= start_date and item.date <= end_date:
-                    founded_lst.append(item)
+
+            is_ransaction_in_range_of_date=lambda item:True if item.date >= start_date and item.date <= end_date else False
+                    
+            founded_lst=filter(is_ransaction_in_range_of_date, list(db.values()))                    
             return founded_lst
 
     def summary_report_between_two_dates_(self, start_date: date, end_date: date):
         with shelve.open(self.file_path)as db:
-            founded_lst = []
             total_income = 0
             total_expence = 0
             income_category = {}
@@ -68,14 +43,3 @@ class TransactionsFileHandler:
                         expence_category[item.category] = expence_category.get(
                             item.category, 0)+1
             return {"total_income": total_income, "income_category": income_category, "total_expence": total_expence, "expence_category": expence_category}
-    # def get_all_values(self):
-    #     with shelve.open(self.file_path)as db:
-    #         return list(db.values())
-
-    # def remove(self, key):
-    #     with shelve.open(self.file_path)as db:
-    #         if key in db.keys():
-    #             del db[key]
-    #             return True
-    #         else:
-    #             return False
--- hw11/transaction/operations.py
+++ hw11/transaction/operations.py
@@ -1,51 +1,40 @@
 from datetime import datetime, date
 from transaction.models import Transaction, TransactionType
 from core.handlers import TransactionsFileHandler
-# , BalanceFileHandler
 
 
 class PersonalFinanceManager:
 
-    __tlb_transaction = TransactionsFileHandler("storage/transactions.shelve")
-    # __tlb_balance = BalanceFileHandler("storage/balance.shelve")
+    __db = TransactionsFileHandler("storage/transactions.shelve")
 
     @classmethod
     def get_all_transaction_from_file(cls):
-        return cls.__tlb_transaction.get_all_transactions()
+        return cls.__db.get_all_transactions()
 
     @classmethod
     def add_transaction(cls, transaction: Transaction) -> None:
         """Add a new transaction to the personal finance manager."""
-        # if transaction.type == TransactionType.INCOME:
-        #     cls.__tlb_balance.set_overal_income_balance(transaction.amount)
 
-        # elif transaction.type == TransactionType.EXPENSE:
-        #     cls.__tlb_balance.set_overal_expence_balance(transaction.amount)
-        # else:
-        #     raise ValueError("Invalid transaction type")
-
-        cls.__tlb_transaction.append_transaction(transaction)
-        # print(cls.__tlb_balance.get_overal_income_balance() -
-        #       cls.__tlb_balance.get_overal_expence_balance())
+        cls.__db.append_transaction(transaction)
 
     @classmethod
     def show_transactions(cls, start_date: date = None, end_date: date = None) -> None:
         """View all transactions between two dates."""
         if start_date == None and end_date == None:  # show all transactions
-            lst_result = cls.__tlb_transaction.get_all_transactions()
+            lst_result = cls.__db.get_all_transactions()
             print(
                 *lst_result, sep="\n") if lst_result else print("No transaction added")
         elif end_date == None:  # user enter just start_date => show transactions until now
             end_date = datetime.now().date()
             start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
-            lst_result = cls.__tlb_transaction.search_transaction_by_date_range(
+            lst_result = cls.__db.search_transaction_by_date_range(
                 start_date, end_date)
             print(
                 *lst_result, sep="\n") if lst_result else print("No transaction in these two ranges of dates")
         else:  # user enter start_date and end_date
             start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
             end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-            lst_result = cls.__tlb_transaction.search_transaction_by_date_range(
+            lst_result = cls.__db.search_transaction_by_date_range(
                 start_date, end_date)
             print(
                 *lst_result, sep="\n") if lst_result else print("No transaction in these two ranges of dates")
@@ -56,14 +45,13 @@ class PersonalFinanceManager:
         if start_date != None and end_date != None:  # show all transactions
             start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
             end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-            dict_result = cls.__tlb_transaction.summary_report_between_two_dates_(
+            dict_result = cls.__db.summary_report_between_two_dates_(
                 start_date, end_date)
             if dict_result.get("income_transaction_count") == 0 and dict_result.get("expence_transaction_count") == 0:
                 print("No Transaction added..")
             else:
                 balance = dict_result["total_income"] - \
                     dict_result["total_expence"]
-                # print(dict_result, f" balance : {balance}")
                 print(
                     f"'total_income': {dict_result['total_income']}, 'income_category': {dict_result['income_category']}, 'total_expence': {dict_result['total_expence']}, 'expence_category': {dict_result['expence_category']}, balance : {balance}")
         else:
