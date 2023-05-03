64dce4aa2e7318d61e11ff07c6e220a01d45628d
Raziye Nikookolah <r.nikookolah@gmail.com>
2023-05-03 19:15:24 +0000

    part 2

--- hw_before_12/dvdrental.sql
+++ hw_before_12/dvdrental.sql
@@ -1,6 +1,17 @@
+#part 1
 select first-name , last-name , count(film-id) as film_count
 from actor 
 join film_actor using(actor-id)
 join film using (film-id)
 order by film_count desc
-limit 10
+limit 10;
+
+
+
+#part 2
+select CONCAT(first-name,' ',last-name), count(film-id) as film_count
+from customer join rental using(customer-id)
+join inventory using(inventory-id)
+join film using (film-id)
+where film_count <> 20 and 'first-name'.startwith('D');
+
