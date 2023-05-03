e2850a8823afdbb7aedf1377a84d4c263f7264f7
Raziye Nikookolah <r.nikookolah@gmail.com>
2023-05-03 19:47:48 +0000

    part 4

--- hw_before_12/dvdrental.sql
+++ hw_before_12/dvdrental.sql
@@ -1,8 +1,8 @@
 #part 1
-select first-name , last-name , count(film-id) as film_count
+select first_name , last_name , count(film_id) as film_count
 from actor 
-join film_actor using(actor-id)
-join film using (film-id)
+join film_actor using(actor_id)
+join film using (film_id)
 group by film_count
 order by film_count desc
 limit 10;
@@ -10,22 +10,33 @@ limit 10;
 
 
 #part 2
-select CONCAT(first-name,' ',last-name), count(film-id) as film_count
-from customer join rental using(customer-id)
-join inventory using(inventory-id)
-join film using (film-id)
-where 'first-name'.startwith('D')
+select CONCAT(first_name,' ',last_name), count(film_id) as film_count
+from customer join rental using(customer_id)
+join inventory using(inventory_id)
+join film using (film_id)
+where first_name.startwith('D')
 group by film_count
 having film_count <> 20 ;
 
 
 #part3
-select name ,count(customer-id) as customer_count
+select name ,count(customer_id) as customer_count
 from category
-join film_category using (category-id)
-join film using (film-id)
-join inventory using (film-id)
-join rental using (inventory-id)
+join film_category using (category_id)
+join film using (film_id)
+join inventory using (film_id)
+join rental using (inventory_id)
 group by customer_count
 order by customer_count desc
 limit 5 offset 5
+
+
+#part 4
+
+select first_name,last_name,actor_id
+from actor join film_actor using(actor_id)
+where first_name=last_name
+
+
+
+
