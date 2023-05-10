4909c76ca7ff606f1a0d1b96013904a8e7684cee
Raziye Nikookolah <r.nikookolah@gmail.com>
2023-05-10 19:31:34 +0000

    part 4 impl Rectangle

--- hw14/q4/test_inventory.py
+++ hw14/q4/test_inventory.py
@@ -24,29 +24,37 @@ def test_add_item(name, quantity):
     assert len(invtry.items) == i
 
 
-def test_remove_item_1():
+def test_remove_item():
     invtry.remove_item("maye_dast", 6)
     assert invtry.items.get("maye_dast") == 24
 
-
-def test_remove_item_2():
     with pytest.raises(KeyError):
         invtry.remove_item("egg", 3)
         print("egg does not exist..")
-
-
-def test_remove_item_3():
     with pytest.raises(ValueError):
         invtry.remove_item("maye_dast", 63)
         print("quantity is less than 63")
 
 
-def test_get_item_quantity_1():
+# def test_remove_item():
+#     with pytest.raises(KeyError):
+#         invtry.remove_item("egg", 3)
+#         print("egg does not exist..")
+
+
+# def test_remove_item():
+#     with pytest.raises(ValueError):
+#         invtry.remove_item("maye_dast", 63)
+#         print("quantity is less than 63")
+
+
+def test_get_item_quantity():
     assert invtry.get_item_quantity("roghan") == 20
-
-
-def test_get_item_quantity_2():
     assert invtry.get_item_quantity("kolah") == 0
+
+
+# def test_get_item_quantity_2():
+#     assert invtry.get_item_quantity("kolah") == 0
 
 
 def test_get_total_items():
---
+++ hw14/q5/rectangle.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self, length, width) -> None:
+        self.length = length
+        self.width = width
+
+    def perimeter():
+        ...
+
+    def area():
+        ...
+
+    def invalid_dimensions():
+        ...
