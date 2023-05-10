08901af4f4b7f80410e514eef55b505eacc1d2c2
Raziye Nikookolah <r.nikookolah@gmail.com>
2023-05-10 12:29:05 +0000

    part 2 completed

--- hw14/q2/sun_event_time_api.py
+++ hw14/q2/sun_event_time_api.py
@@ -2,22 +2,25 @@ import requests
 import datetime
 
 
-def get_exact_time_for_iran(api_time):  # add 2:30 to time recieved
-    time_object = datetime.datetime.strptime(api_time, "%H:%M:%S")
-    delta_time = datetime.timedelta(minutes=30, hours=2)
-    return time_object + delta_time
+def get_local_time(api_time):  # add 3:30 to time
+    time_object = datetime.datetime.strptime(api_time, "%I:%M:%S %p")
+    delta_time = datetime.timedelta(minutes=30, hours=3)
+    return (time_object + delta_time).time()
 
 
 url = "https://api.sunrise-sunset.org/json"
-
-parameters = {"lat": "29.62217", "lng": "52.54688", "date": "today"}
+parameters = {
+    "lat": "29.62217",
+    "lng": "52.54688",
+    "date": "today",
+}
 r = requests.get(url, parameters)
-# print(r.status_code)
+if r.status_code == 200:
+    data = r.json()
 
-data = r.json()
+    sunrise_time = get_local_time(data["results"]["sunrise"])
+    sunset_time = get_local_time(data["results"]["sunset"])
 
-sunrise_time = get_exact_time_for_iran(data["results"]["sunrise"].split()[0])
-sunset_time = get_exact_time_for_iran(data["results"]["sunset"].split()[0])
-
-print(f"sunrise:{sunrise_time}")
-print(f"sunset:{sunset_time}")
+    print(f"sunrise:{sunrise_time} and sunset:{sunset_time}")
+else:
+    print("Error ..")
