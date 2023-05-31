2ce807ccdbd04cbdfca016e1a8ba7bbf4c2cc291
Raziye Nikookolah <r.nikookolah@gmail.com>
2023-05-31 11:42:07 +0000

    modify model fields

--- hw17/q/app/model.py
+++ hw17/q/app/model.py
@@ -5,18 +5,23 @@ class PostSchema(BaseModel):
     id: int = Field(default=None)
     title: str = Field(default=None)
     content: str = Field(default=None)
+    author: str = Field(default=None)
 
     class Config:
         schema_extra = {
             "example": {
-                "post_schema": {"title": "some title", "content": "description"}
+                "post_schema": {
+                    "title": "Hiro",
+                    "content": "Hiro description",
+                    "author": "raziye",
+                }
             }
         }
 
 
 class UserSchema(BaseModel):
     id: int = Field(default=None)  # it can be ... in paranthesese
-    fullname: int = Field(default=None)
+    username: int = Field(default=None)
     email: EmailStr = Field(default=None)
     password: str = Field(default=None)
 
@@ -24,7 +29,7 @@ class UserSchema(BaseModel):
         schema_extra = {
             "example": {
                 "user_schema": {
-                    "fullname": "raziye nikookolah",
+                    "username": "raziye nikookolah",
                     "email": "r.nikookolah@gmail.com",
                     "password": "123",
                 }
