99c5109eec3419b6fb3bcd34902b3eb5750874a2
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-14 22:39:04 -0600

    change the arguments

--- hw19/q/main.py
+++ hw19/q/main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, status, HTTPException, Query
+from fastapi import FastAPI, status, HTTPException, Query, Path
 import uvicorn
 from pydantic import BaseModel
 from enum import Enum
-from datetime import datetime
+from datetime import date, datetime
 from typing import Annotated
 from pymongo import MongoClient
-import pymongo
 from bson.objectid import ObjectId
 
 
@@ -13,19 +12,18 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client['transaction']
 
 transaction_collection = db['transaction']
-# transaction_collection.create_index('merchantId', unique=True)
-# transaction_collection.create_index(
-#     [('merchantId', pymongo.TEXT)], name='user_id_index', default_language='english')
 
 collections_in_db = db.list_collection_names()
 
-# Check if the collection exists in the list of collection names
-if not 'transaction_user_day_collection' in collections_in_db:
+# if not 'transaction_user_day_collection' in collections_in_db:
+
+transaction_user_day_collection = db['transaction_user_day_collection']
+if transaction_user_day_collection.find_one() is None:
     pipeline = [
         {
             "$group": {
                 "_id": {
-                    "merchantId": "$merchantId",
+                    "merchantId": {"$toString": "$merchantId"},
                     "createdAt": {
                         "$dateToString": {
                             "format": "%Y-%m-%d",
@@ -44,85 +42,12 @@ if not 'transaction_user_day_collection' in collections_in_db:
         }
     ]
 
-    # Execute the pipeline and print the results
     result = db.transaction.aggregate(pipeline)
 
-    transaction_collection = db['transaction_user_day_collection']
-    transaction_collection.insert_many(result)
-
-
-class TimePeriod(str, Enum):
-    WEEK = "week"
-    DAY = "day"
-    MONTH = "month"
-    YEAR = "year"
-
-
-def get_transactions():
-    transactions = list(transaction_collection.find())
-    for trnsc in transactions:
-        trnsc["_id"] = str(trnsc["_id"])
-        trnsc["merchantId"] = str(trnsc["merchantId"])
-
-    return transactions
-
-
-def search(type: TimePeriod, day: datetime, input: str):
-    docs = list(transaction_collection.find({f'{type}': input}))
-    for trnsc in docs:
-        trnsc["_id"] = str(trnsc["_id"])
-        trnsc["merchantId"] = str(trnsc["merchantId"])
-    return docs
-
-
-def get_by_id(id: str):
-    trnsc = transaction_collection.find_one({"_id": ObjectId(id)})
-    trnsc["_id"] = str(trnsc["_id"])
-    trnsc["merchantId"] = str(trnsc["merchantId"])
-    return trnsc
-
-
-def get_by_merchant_id(id: str):
-    trnscs = list(transaction_collection.find({"merchantId": ObjectId(id)}))
-    for trnsc in trnscs:
-        trnsc["_id"] = str(trnsc["_id"])
-        trnsc["merchantId"] = str(trnsc["merchantId"])
-
-    return trnscs
-
-
-class TransactionInDB(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
-    _id: ObjectId
-    merchantId: ObjectId
-    amount: int
-    createdAt: datetime
+    transaction_user_day_collection.insert_many(result)
 
 
 app = FastAPI()
-
-
-@app.get(
-    "/{trnsc_id}"  # , response_model=TransactionInDB
-)
-def get_transaction_by_transc_id(id: str):
-    # transc = TransactionInDB(**get_by_id(id))
-    transc = get_by_merchant_id(id)
-    if not transc:
-        raise HTTPException(status.HTTP_404_NOT_FOUND)
-    return transc
-
-
-@app.get(
-    "/{merchant_id}"  # , response_model=TransactionInDB
-)
-def get_transaction_by_merchant_id(id: str):
-    # transc = TransactionInDB(**get_by_id(id))
-    transc = get_by_merchant_id(id)
-    if not transc:
-        raise HTTPException(status.HTTP_404_NOT_FOUND)
-    return transc
 
 
 @app.get("/")
@@ -130,13 +55,97 @@ async def main():
     return {"message": "Hello World"}
 
 
-@app.post(
+@app.get(
     "/search"
 )
-def search_transaction(search_item: Annotated[str, Query(..., enum=["week", "day", "month", "year"])], input: str):
-    result = search(search_item, input)
-    return result
+def search_transaction(
+        search_item: Annotated[str, Path(..., enum=["week", "day", "month", "year"])],
+        amount_or_count: Annotated[str, Path(..., enum=["amount", "count"])],
+        tomans_or_rials: Annotated[str, Path(..., enum=["rials", "tomans"])],
+        date_from: str,  # Annotated[str, Path(...)],
+        date_to: str
+        # merchant_id: Annotated[str, Path(...)]
+):
+    print(date_from, date_to, 111111111111111111)
+    date_from = datetime.strptime(date_from, "%Y-%m-%d").date().isoformat()
+    date_to = datetime.strptime(date_to, "%Y-%m-%d").date().isoformat()
+
+    # if merchant_id:
+    #     if not ObjectId.is_valid(merchant_id):
+    #         raise HTTPException(status_code=422, detail="Invalid merchant_id")
+
+    docs = list(transaction_user_day_collection.find({
+        "_id.createdAt": {"$gte": f'{date_from}', "$lt": f'{date_to}'}
+
+    }))
+    for trnsc in docs:
+        print(trnsc)
+
+    return docs
 
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", port=8000, reload=True, host="127.0.0.1")
+
+# def get_transactions():
+#     transactions = list(transaction_collection.find())
+#     for trnsc in transactions:
+#         trnsc["_id"] = str(trnsc["_id"])
+#         trnsc["merchantId"] = str(trnsc["merchantId"])
+
+#     return transactions
+
+
+# def search(type: TimePeriod, day: datetime, input: str):
+#     docs = list(transaction_collection.find({f'{type}': input}))
+#     for trnsc in docs:
+#         trnsc["_id"] = str(trnsc["_id"])
+#         trnsc["merchantId"] = str(trnsc["merchantId"])
+#     return docs
+
+
+# def get_by_id(id: str):
+#     trnsc = transaction_collection.find_one({"_id": ObjectId(id)})
+#     trnsc["_id"] = str(trnsc["_id"])
+#     trnsc["merchantId"] = str(trnsc["merchantId"])
+#     return trnsc
+
+
+# def get_by_merchant_id(id: str):
+#     trnscs = list(transaction_collection.find({"merchantId": ObjectId(id)}))
+#     for trnsc in trnscs:
+#         trnsc["_id"] = str(trnsc["_id"])
+#         trnsc["merchantId"] = str(trnsc["merchantId"])
+
+#     return trnscs
+
+
+# class TransactionInDB(BaseModel):
+#     class Config:
+#         arbitrary_types_allowed = True
+#     _id: ObjectId
+#     merchantId: ObjectId
+#     amount: int
+#     createdAt: datetime
+
+
+# @app.get(
+#     "/{trnsc_id}"  # , response_model=TransactionInDB
+# )
+# def get_transaction_by_transc_id(id: str):
+#     # transc = TransactionInDB(**get_by_id(id))
+#     transc = get_by_merchant_id(id)
+#     if not transc:
+#         raise HTTPException(status.HTTP_404_NOT_FOUND)
+#     return transc
+
+
+# @app.get(
+#     "/{merchant_id}"  # , response_model=TransactionInDB
+# )
+# def get_transaction_by_merchant_id(id: str):
+#     # transc = TransactionInDB(**get_by_id(id))
+#     transc = get_by_merchant_id(id)
+#     if not transc:
+#         raise HTTPException(status.HTTP_404_NOT_FOUND)
+#     return transc
