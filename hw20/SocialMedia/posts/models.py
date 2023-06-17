a115177d92ed716c0ba8b34786936ca0aa3b8f89
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-16 19:29:25 -0600

    impl address model

--- hw20/SocialMedia/posts/models.py
+++ hw20/SocialMedia/posts/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+
+class Address(models.Model):
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    street_name = models.CharField(max_length=100)
