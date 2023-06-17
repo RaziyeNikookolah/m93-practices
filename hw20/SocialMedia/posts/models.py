fa178cee15116a60093f0469ab50219daedd4726
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-17 12:12:35 -0600

    impl __str__ method for all models

--- hw20/SocialMedia/posts/models.py
+++ hw20/SocialMedia/posts/models.py
@@ -7,6 +7,9 @@ class User(models.Model):
     phone_number = models.CharField(max_length=13)
     national_code = models.CharField(max_length=10)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Address(models.Model):
     country = models.CharField(max_length=100)
@@ -15,6 +18,9 @@ class Address(models.Model):
     street_name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_addresses")
 
+    def __str__(self):
+        return f'{self.city}'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
@@ -22,11 +28,14 @@ class Post(models.Model):
     image = models.ImageField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
 
+    def __str__(self):
+        return f'{self.title}'
+
 
 class Comment(models.Model):
     body = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments')
 
-
-
+    def __str__(self):
+        return f'{self.body}'
