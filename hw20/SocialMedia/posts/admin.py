57d8f18d182f1551e0be4a77e8538a2e9185cfee
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-17 13:24:26 -0600

    set models to admin.py

--- hw20/SocialMedia/posts/admin.py
+++ hw20/SocialMedia/posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, User, Comment, Address
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(Address)
