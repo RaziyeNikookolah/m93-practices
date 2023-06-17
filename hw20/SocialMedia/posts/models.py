5d051a897dffdbd9cc0d64118ecc6cf4f111e4d4
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-16 19:30:02 -0600

    impl User model

--- hw20/SocialMedia/posts/models.py
+++ hw20/SocialMedia/posts/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class User(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=13)
+    national_code = models.CharField(max_length=10)
+    address = models.ForeignKey(
+        "Address",
+        on_delete=models.CASCADE,
+    )
+
+
 class Address(models.Model):
     country = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
