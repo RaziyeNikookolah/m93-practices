f0b5085be83ddb9e3d95b8d07806f42e2bb2e969
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-19 10:08:02 +0330

    put each model in its own app

--- hw20/SocialMedia/posts/admin.py
+++ hw20/SocialMedia/posts/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Post, User, Comment, Address
+from .models import Post, Comment
 
 admin.site.register(Post)
-admin.site.register(User)
 admin.site.register(Comment)
-admin.site.register(Address)
--- hw20/SocialMedia/posts/models.py
+++ hw20/SocialMedia/posts/models.py
@@ -1,31 +1,5 @@
 from django.db import models
-
-
-class User(models.Model):
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    phone_number = models.CharField(max_length=13)
-    national_code = models.CharField(max_length=10)
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-    class Meta:
-        verbose_name_plural = 'Users'
-
-
-class Address(models.Model):
-    country = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    street_name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_addresses")
-
-    def __str__(self):
-        return f'{self.city} {self.street_name}'
-
-    class Meta:
-        verbose_name_plural = 'Addresses'
+from accounts.models import User, Address
 
 
 class Post(models.Model):
