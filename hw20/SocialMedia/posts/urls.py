1e0b442c3dd7f47c0ba47b3ebb6d506b3a051a96
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-19 13:56:50 +0330

    impl all_posts.html

--- hw20/SocialMedia/posts/urls.py
+++ hw20/SocialMedia/posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index)
-
+    path('all_posts/', views.all_posts,name='all_posts'),
+    path('post_details/<int:post_id>/', views.post_details, name='post_details'),
 ]
--- hw20/SocialMedia/posts/views.py
+++ hw20/SocialMedia/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
+
+def post_details(request, post_id):
+    if request.method == 'GET':
+        post = Post.objects.get(id=post_id)
+        return render(request, 'posts/post_details.html', {'post': post})
+
+
+def all_posts(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        return render(request, 'posts/all_posts.html', {"posts": posts})
