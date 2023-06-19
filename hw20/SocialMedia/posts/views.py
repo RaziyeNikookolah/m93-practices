585a26d81be30548aca86f85879d48b8ff79f9b6
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-19 04:41:07 -0600

    add html code to templates files

--- hw20/SocialMedia/posts/views.py
+++ hw20/SocialMedia/posts/views.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render
-from .forms import PostCreateForm
 from django.http import HttpResponse
 
 
-# Create your views here.
-def create(request):
-    if request.method == 'POST':
-        pass
-    elif request.method == 'GET':
-        form = PostCreateForm()
-        return render(request, 'home/create.html', {'form': form})
-
-
 def index(request):
-    return  HttpResponse("<h3>heloooo</h3>")
+    return HttpResponse("<h3>heloooo</h3>")
