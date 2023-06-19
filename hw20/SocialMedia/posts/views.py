d4cbd840a57dc4c69457b7e1cfc242c0af73e2dd
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-19 15:41:21 +0330

    correct the code in post_details.html in comments part

--- hw20/SocialMedia/posts/views.py
+++ hw20/SocialMedia/posts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post,Comment
 from accounts.models import User
 
 
 def post_details(request, post_id):
     if request.method == 'GET':
         post = Post.objects.get(id=post_id)
-        return render(request, 'posts/post_details.html', {'post': post})
+        comments=Comment.objects.filter(post=post)
+        return render(request, 'posts/post_details.html', {'post': post,"comments":comments})
 
 
 def all_posts(request):
