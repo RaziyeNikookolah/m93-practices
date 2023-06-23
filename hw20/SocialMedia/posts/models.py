ab95491401bb207bf636244b34a526988bae1cc3
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-23 06:00:31 +0330

    add a fields to comment and post model

--- hw20/SocialMedia/posts/models.py
+++ hw20/SocialMedia/posts/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from accounts.models import User
 
 
-class Post(models.Model):#TODO it can have a date
+class Post(models.Model):  # TODO it can have a date
     title = models.CharField(max_length=250)
     body = models.TextField(blank=True, null=True)
     image = models.ImageField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+    createdAt = models.DateField(auto_now_add=True)
+    isActive = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.title}'
@@ -19,6 +21,8 @@ class Comment(models.Model):
     body = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments')
+    createdAt = models.DateField(auto_created=True)
+    isActive=models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.author} says: {self.body} '
